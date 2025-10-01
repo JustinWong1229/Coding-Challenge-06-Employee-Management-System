2bd769d486f37e6898025c0ba9c37d9ade6f0c0d
Justin Wong <jcwong5@usf.edu>
2025-10-01 11:19:26 -0400

    Created a subclass that extends the previous class and uses super () to inherit the methods of the pervious class.

--- cc_6.js
+++ cc_6.js
@@ -1,4 +1,5 @@
-class Employee {
+// Step 2: Creating a Class
+class Employees {
     constructor (name, deparment) {
         this.name = name
         this.deparment = deparment
@@ -7,3 +8,13 @@ class Employee {
         `${this.name} works in the ${this.deparment}.`
     }
 } 
+// Step 3: Creating a subclass
+class Manager extends Employees {
+    constructor (name, deparment, teamSize) {
+        super (name, deparment)
+        this.teamSize = teamSize
+    }
+    describe () {
+        `${this.name} is the manager for the ${this.deparment} and has a team size of ${this.teamSize}.`
+    }
+}
